9a5e23a189e14ca54a9300a7c28df12168174abe
UserHab <vitalibembel1.21@gmail.com>
2022-04-21 15:07:58 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import menu.Menu;
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.IntroducingMenu();
+    }
+}
+
